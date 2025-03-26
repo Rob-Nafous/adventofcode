a481eb5b59cc17d02bc46feb0ab967846cbc518e
Robin Bonafous <robin.bonafous@gmail.com>
2025-03-26 09:57:19 +0100

    Update Day4.py

--- 2024/Day4.py
+++ 2024/Day4.py
@@ -7,7 +7,7 @@ x = [[*map(str, line.split())] for line in content.split('\n') if line.strip()]
 separated_matrix = [[char for char in row[0]] for row in x]
 
 def check_around2(x, y, matrix):
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    directions = [(0, 1),(0, -1),(1, 0),(-1, 0),(1, 1),(1, -1),(-1, 1),(-1, -1)]
     howmany = 0
 
     for dx, dy in directions:
@@ -24,15 +24,62 @@ def check_around2(x, y, matrix):
 
     return howmany
 
+
+# Part 2
+def check_aroundX_MAS(x, y, matrix):
+    diag1 = [(-1, -1), (1, 1)]  # /
+    diag2 = [(-1, 1), (1, -1)]  # \
+    confirm = 0
+    chars_diag1 = []
+    for dx, dy in diag1:
+        new_x, new_y = x + dx, y + dy
+        if 0 <= new_x < len(matrix) and 0 <= new_y < len(matrix[0]):
+            chars_diag1.append(matrix[new_x][new_y])
+        else:
+            break 
+
+    if set(chars_diag1) != {'M', 'S'}:
+        pass
+    else:
+        confirm += 1
+
+    chars_diag2 = []
+    for dx, dy in diag2:
+        new_x, new_y = x + dx, y + dy
+        if 0 <= new_x < len(matrix) and 0 <= new_y < len(matrix[0]):
+            chars_diag2.append(matrix[new_x][new_y])
+        else:
+            break
+
+    if set(chars_diag2) != {'M', 'S'}:
+        pass
+    else:
+        confirm += 1
+    if confirm == 2:
+        return 1
+    else:
+        return 0
+
+
+
 count = 0
-
-
+#check XMAS
 for row_idx, line in enumerate(separated_matrix):
     for col_idx, char in enumerate(line):
         if char == 'X':
             new_count = check_around2(row_idx, col_idx, separated_matrix)
             if new_count > 0:
                 count += new_count
-                print(count, row_idx, col_idx, new_count)  
-
+                #print(count, row_idx, col_idx, new_count)  
 print("Final Count:", count)
+
+#check X-MAS
+count = 0
+for row_idx, line in enumerate(separated_matrix):
+    for col_idx, char in enumerate(line):
+        if char == 'A':
+            new_count = check_aroundX_MAS(row_idx, col_idx, separated_matrix)
+            if new_count > 0:
+                count += new_count
+                #print(count, row_idx, col_idx, new_count)
+print("X-MAS Count:", count)
